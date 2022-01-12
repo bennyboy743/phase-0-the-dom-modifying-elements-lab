52d1fed78af2c1543efb8bba8285adead3efd090
Benjamin <benjaminrowe0@gmail.com>
2022-01-12 22:48:04 +1100

    have solved the problem

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+//getting the ID that we need for the main
+const removingMain = document.getElementById('main')
+console.log(removingMain)
+
+//removing the main 
+removingMain.remove()
+
+//making a new header 
+const newHeader = document.createElement('h1')
+
+//change the ID to victory
+newHeader.id = 'victory'
+// then we write to the innerHTML
+newHeader.innerHTML = "Ben is the champion";
+
+
+
+
+
+//then going to add it to the DOM
+document.body.append(newHeader)
